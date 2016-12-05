f092879eadc3813c64f80c3b896098d6ce500a81
John Brock <johnlrbrock@gmail.com>
2016-12-05 12:24:40 -0500

    final version. Game restarts if correct answer or runs out of turns. Added cheat mode.

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,45 +1,65 @@
 require 'sinatra'
 require 'sinatra/reloader'
-number = rand(101)
+@@number = rand(101)
 secret_number_line = ''
+@@guesses = 5
 get '/' do
-  guess = params['guess']
-  message = check_guess(guess, number)
-  secret_number_line = correct_guess?(guess, number)
-  style = style(message)
+  guess = params['guess'].nil? ? nil : params['guess'].to_i
+  cheat_mode = cheat_mode? ? "The number is #{@@number}." : ''
+  message = check_guess(guess)
+  style = style(guess)
   erb :index, :locals => { :message => message, 
-                           :secret_number_line => secret_number_line, 
-                           :style => style
-                          }
+                           :style => style,
+                           :cheat_mode => cheat_mode
+                         }
 end
 
-def check_guess(guess, number)
-  if params['guess'].nil?
-    message = 'Guess my mystery number.'
-  elsif params['guess'].to_i < number - 5
-    message = 'Way too low!'
-  elsif params['guess'].to_i > number + 5
-    message = 'Way too high!'
-  elsif params['guess'].to_i > number
-    message = 'Too high!'
-  elsif params['guess'].to_i < number
-    message = 'Too low!'
-  elsif params['guess'].to_i == number
-    message = 'You got it right!'
+
+def check_guess(guess)
+  @@correct = false
+  if guess.nil?
+    message = 'Guess my mystery number. You have 5 turns.'
+  elsif correct_guess?(guess)
+    @@correct = true
+    @@guesses = 5
+    @@number = rand(101)
+    'You got it right! I have a new number. You have 5 turns.'
+  elsif @@guesses < 2
+    @@guesses = 5
+    @@number == rand(101)
+    'You ran out of guesses! I have a new number. You have 5 turns.'
+  elsif guess < @@number - 5
+    @@guesses -= 1
+    "Way too low! You have #{@@guesses} turns."
+  elsif guess > @@number + 5
+    @@guesses -= 1
+    "Way too high! You have #{@@guesses} turns."
+  elsif guess > @@number
+    @@guesses -= 1
+    "Too high! You have #{@@guesses} turns."
+  elsif guess < @@number
+    @@guesses -= 1
+    "Too low! You have #{@@guesses} turns."
   end
 end
 
-def correct_guess?(guess, number)
-  params['guess'].to_i == number ? secret_number_line = "The secret number was #{number}." : ''
+def correct_guess?(guess)
+  guess == @@number
 end
 
-def style(message)
-  if message == 'Way too high!' || message == 'Way too low!'
-    color = '#f02'
-  elsif message == 'Too low!' || message == 'Too high!'
-    color = '#f99'
-  elsif message == 'You got it right!'
+def style(guess)
+  if @@correct
     color = '#0f0'
+  elsif params['guess'].nil?
+    color = '#99f'
+  elsif guess < @@number - 5 || guess > @@number + 5
+    color = '#f02'
+  elsif guess < @@number || guess > @@number
+    color = '#f99'
   end
  "body {\nbackground: #{color};\n}"
+end
+
+def cheat_mode?
+  params['cheat']
 end
